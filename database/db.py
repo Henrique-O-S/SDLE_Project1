f8b5c6bace813422f6647f311db12224b4707467
Henrique Silva <up202007242@fe.up.pt>
2023-10-17 09:06:49 +0100

    Added database script

---
+++ database/db.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+# Connect to the database (or create it if it doesn't exist)
+conn = sqlite3.connect('database/shopping_list.db')
+
+# Create a cursor object
+cursor = conn.cursor()
+
+# Create a table and define its schema
+
+cursor.execute('''
+    DROP TABLE IF EXISTS shopping_list;
+    DROP TABLE IF EXISTS item;
+''')
+
+# Commit the changes and close the connection
+conn.commit()
+conn.close()
