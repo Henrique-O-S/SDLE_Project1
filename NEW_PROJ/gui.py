637de1e845efbc9d82da130cfae5341f6a384c7a
DiogoSilva11 <up202004288@up.pt>
2023-11-27 17:45:41 +0000

    gui updates

--- NEW_PROJ/gui.py
+++ NEW_PROJ/gui.py
@@ -1,159 +1,232 @@
+# --------------------------------------------------------------
+
 import tkinter as tk
 
-class ShoppingListApp:
+# --------------------------------------------------------------
+
+class ArmazonGUI:
     def __init__(self, root):
         self.root = root
-        self.root.title("Shopping List Application")
-        self.header = tk.Label(self.root, text="Shopping List Application", font=("Arial", 18, "bold"), bg="#3498db", fg="white", pady=15)
-        self.header.pack(fill=tk.X)
-        self.home_btn = tk.Button(self.root, text="Home Page", font=("Arial", 10), command=self.show_home)
-        self.home_btn.pack(pady=10)
-        self.admin_btn = tk.Button(self.root, text="Admin Page", font=("Arial", 10), command=self.show_admin)
-        self.admin_btn.pack(pady=10)
-        self.content_frame = tk.Frame(self.root)
-        self.content_frame.pack(padx=100, pady=50)
-        self.shopping_lists = [{"name": "Groceries", "items": [{"name": "Apples", "quantity": 3}, {"name": "Milk", "quantity": 2}, {"name": "Bread", "quantity": 1}]}]
-        self.show_home()
+        self.root.title("ARMAZON")
+        self.shopping_lists = [
+            {
+                "id": 0,
+                "name": "Groceries",
+                "items": [
+                    {
+                        "name": "Apples",
+                        "quantity": 3
+                    },
+                    {
+                        "name": "Milk",
+                        "quantity": 2
+                    },
+                    {
+                        "name": "Bread",
+                        "quantity": 1
+                    }
+                ]
+            }    
+        ]
+        self.build()
 
-    def clear_content_frame(self):
+    def clear(self):
         for widget in self.content_frame.winfo_children():
             widget.destroy()
 
-    def show_home(self):
-        self.clear_content_frame()
+    def build(self):
+        self.header()
+        self.home_button()
+        self.admin_button()
+        self.frame()
+        self.home()
+
+    def frame(self):
+        self.content_frame = tk.Frame(self.root)
+        self.content_frame.pack(padx=100, pady=10)
+
+# --------------------------------------------------------------
+
+    def header(self):
+        self.header = tk.Label(self.root, text="ARMAZON", font=("Arial", 18, "bold"), bg="#3498db", fg="white", pady=15)
+        self.header.pack(fill=tk.X)
+
+    def home_button(self):
+        self.home_btn = tk.Button(self.root, text="Home Page", font=("Arial", 10), command=self.home)
+        self.home_btn.pack(pady=10)
+
+    def admin_button(self):	
+        self.admin_btn = tk.Button(self.root, text="Admin Page", font=("Arial", 10), command=self.admin)
+        self.admin_btn.pack(pady=10)
+
+# --------------------------------------------------------------
+
+    def home(self):
+        self.clear()
         label = tk.Label(self.content_frame, text="Home Page", font=("Arial", 14, "bold"))
         label.pack()
+        self.create_list_input()
+        self.view_list_input()
+
+    def create_list_input(self):
         new_list_label = tk.Label(self.content_frame, text="Create New Shopping List:")
         new_list_label.pack()
         self.new_list_entry = tk.Entry(self.content_frame)
         self.new_list_entry.pack()
         add_list_button = tk.Button(self.content_frame, text="Add List", command=self.add_shopping_list)
         add_list_button.pack()
-        view_list_label = tk.Label(self.content_frame, text="View Shopping List:")
-        view_list_label.pack()
-        self.view_list_entry = tk.Entry(self.content_frame)
-        self.view_list_entry.pack()
-        view_list_button = tk.Button(self.content_frame, text="View List", command=self.view_shopping_list)
-        view_list_button.pack()
-
-    def show_admin(self):
-        self.clear_content_frame()
-        label = tk.Label(self.content_frame, text="Admin Page", font=("Arial", 14, "bold"))
-        label.pack()
-        if self.shopping_lists:
-            list_label = tk.Label(self.content_frame, text="All Shopping Lists:")
-            list_label.pack()
-            for idx, shopping_list in enumerate(self.shopping_lists, start=1):
-                list_button = tk.Button(self.content_frame, text=f"{idx}. {shopping_list['name']}", command=lambda idx=idx: self.delete_shopping_list(idx))
-                list_button.pack()
-        else:
-            empty_label = tk.Label(self.content_frame, text="No shopping lists available.")
-            empty_label.pack()
 
     def add_shopping_list(self):
         new_list_name = self.new_list_entry.get()
         if new_list_name:
-            self.shopping_lists.append({"name": new_list_name, "items": []})
+            new_list_id = len(self.shopping_lists)
+            self.shopping_lists.append({"id": new_list_id, "name": new_list_name, "items": []})
             self.new_list_entry.delete(0, tk.END)
-            self.show_admin()
+            self.admin()
 
-    def view_shopping_list(self):
-        list_id = self.view_list_entry.get()
-        if list_id and list_id.isdigit():
-            list_id = int(list_id)
-            if 1 <= list_id <= len(self.shopping_lists):
-                shopping_list = self.shopping_lists[list_id - 1]
-                self.clear_content_frame()
-                label = tk.Label(self.content_frame, text=f"Shopping List: {shopping_list['name']}", font=("Arial", 14, "bold"))
-                label.pack()
-                items_label = tk.Label(self.content_frame, text="Items:")
-                items_label.pack()
-                for idx, item in enumerate(shopping_list['items'], start=1):
-                    item_label = tk.Label(self.content_frame, text=f"{idx}. {item['name']} - Quantity: {item['quantity']}")
-                    item_label.pack()
-                add_item_label = tk.Label(self.content_frame, text="Add Item:")
-                add_item_label.pack()
-                self.new_item_name_entry = tk.Entry(self.content_frame)
-                self.new_item_name_entry.pack()
-                self.new_item_quantity_entry = tk.Entry(self.content_frame)
-                self.new_item_quantity_entry.pack()
-                add_item_button = tk.Button(self.content_frame, text="Add Item", command=lambda: self.add_item(list_id))
-                add_item_button.pack()
-                modify_item_label = tk.Label(self.content_frame, text="Modify Item Quantity:")
-                modify_item_label.pack()
-                self.modify_item_index_entry = tk.Entry(self.content_frame)
-                self.modify_item_index_entry.pack()
-                self.modify_item_quantity_entry = tk.Entry(self.content_frame)
-                self.modify_item_quantity_entry.pack()
-                modify_item_button = tk.Button(self.content_frame, text="Modify Item Quantity", command=lambda: self.modify_item_quantity(list_id))
-                modify_item_button.pack()
-                delete_item_label = tk.Label(self.content_frame, text="Delete Item:")
-                delete_item_label.pack()
-                self.delete_item_index_entry = tk.Entry(self.content_frame)
-                self.delete_item_index_entry.pack()
-                delete_item_button = tk.Button(self.content_frame, text="Delete Item", command=lambda: self.delete_item(list_id))
-                delete_item_button.pack()
-            else:
-                error_label = tk.Label(self.content_frame, text="Invalid shopping list ID")
-                error_label.pack()
-                self.root.after(1000, error_label.destroy)
+    def view_list_input(self):
+        view_list_label = tk.Label(self.content_frame, text="View Shopping List:")
+        view_list_label.pack()
+        self.view_list_entry = tk.Entry(self.content_frame)
+        self.view_list_entry.pack()
+        view_list_button = tk.Button(self.content_frame, text="View List", command=self.get_view_list_entry)
+        view_list_button.pack()
+
+    def get_view_list_entry(self):
+        list_id = int(self.view_list_entry.get())
+        self.shopping_list(list_id)
+
+# --------------------------------------------------------------
+
+    def admin(self):
+        self.clear()
+        label = tk.Label(self.content_frame, text="Admin Page", font=("Arial", 14, "bold"))
+        label.pack()
+        self.lists()
+
+    def lists(self):
+        if self.shopping_lists:
+            for shopping_list in self.shopping_lists:
+                self.shopping_list_item(shopping_list)
         else:
-            error_label = tk.Label(self.content_frame, text="Please enter a valid ID")
+            empty_label = tk.Label(self.content_frame, text="No shopping lists available.")
+            empty_label.pack()
+
+    def shopping_list_item(self, shopping_list):
+        id = shopping_list["id"]
+        list_button = tk.Button(self.content_frame, text=f"{id}. {shopping_list['name']}", command=lambda: self.shopping_list(shopping_list['id']))
+        list_button.pack()
+        self.delete_list_button(shopping_list['id'])
+
+    def delete_list_button(self, id):
+        delete_button = tk.Button(self.content_frame, text="Delete List", command=lambda: self.delete_list(id))
+        delete_button.pack()
+    def delete_list(self, id):
+        del self.shopping_lists[id]
+        self.admin()
+
+# --------------------------------------------------------------
+
+    def shopping_list(self, list_id):
+        if self.get_list(list_id):
+            shopping_list = self.get_list(list_id)
+            self.clear()
+            label = tk.Label(self.content_frame, text=f"Shopping List: {shopping_list['name']}", font=("Arial", 14, "bold"))
+            label.pack()
+            self.items(shopping_list)
+            self.add_item_input(shopping_list)
+            self.delete_list_button(shopping_list['id'])
+        else:
+            error_label = tk.Label(self.content_frame, text="Shopping list not found.")
+            error_label.pack()
+            self.root.after(1000, error_label.destroy)
+    
+    def items(self, shopping_list):
+        self.item_entries = {}
+        for item in shopping_list['items']:
+            self.item(shopping_list, item)
+
+    def item(self, shopping_list, item):
+        item_label = tk.Label(self.content_frame, text=f"{item['name']}")
+        item_label.pack()
+        self.update_item_input(shopping_list, item) 
+
+    def update_item_input(self, shopping_list, item):
+        self.item_entries[item['name']] = tk.Entry(self.content_frame) 
+        self.item_entries[item['name']].pack()
+        self.item_entries[item['name']].insert(0, str(int(item['quantity'])))
+        modify_item_button = tk.Button(self.content_frame, text="Update Quantity", command=lambda: self.update_item(shopping_list, item))
+        modify_item_button.pack()
+
+    def update_item(self, shopping_list, item):
+        quantity_value = self.item_entries[item['name']].get()  
+        if quantity_value.isdigit() and int(quantity_value) >= 0:
+            for i in range(len(shopping_list['items'])):
+                if shopping_list['items'][i]['name'] == item['name']:
+                    shopping_list['items'][i]['quantity'] = int(quantity_value)
+                    self.shopping_list(shopping_list['id'])
+                    break
+        else:
+            error_label = tk.Label(self.content_frame, text="Please enter a valid quantity")
             error_label.pack()
             self.root.after(1000, error_label.destroy)
 
-    def add_item(self, list_id):
-        shopping_list = self.shopping_lists[list_id - 1]
+    def delete_item_button(self, shopping_list, item):
+        delete_button = tk.Button(self.content_frame, text="Delete", command=lambda: self.delete_item(shopping_list, item))
+        delete_button.pack()
+
+    def delete_item(self, shopping_list, item):
+        for i in range(len(shopping_list['items'])):
+            if shopping_list['items'][i]['name'] == item['name']:
+                del shopping_list['items'][i]
+                self.shopping_list(shopping_list['id'])
+                break
+    
+    def add_item_input(self, shopping_list):
+        label = tk.Label(self.content_frame, text="Add Item:", font=("Arial", 12, "bold"))
+        label.pack()
+        name_label = tk.Label(self.content_frame, text="Name:")
+        name_label.pack()
+        self.new_item_name_entry = tk.Entry(self.content_frame)
+        self.new_item_name_entry.pack()
+        quantity_label = tk.Label(self.content_frame, text="Quantity:")
+        quantity_label.pack()
+        self.new_item_quantity_entry = tk.Entry(self.content_frame)
+        self.new_item_quantity_entry.pack()
+        add_item_button = tk.Button(self.content_frame, text="Add Item", command=lambda: self.add_item(shopping_list))
+        add_item_button.pack()
+
+    def add_item(self, shopping_list):
         new_item_name = self.new_item_name_entry.get()
         new_item_quantity = self.new_item_quantity_entry.get()
-        if new_item_name and new_item_quantity.isdigit():
-            shopping_list['items'].append({"name": new_item_name, "quantity": int(new_item_quantity)})
-            self.view_shopping_list()
+        if new_item_name and new_item_quantity.isdigit() and int(new_item_quantity) >= 0:
+            for i in range(len(self.shopping_lists)):
+                if self.shopping_lists[i]['id'] == shopping_list['id']:
+                    self.shopping_lists[i]['items'].append({"name": new_item_name, "quantity": int(new_item_quantity)})
+                    self.shopping_list(shopping_list['id'])
+                    break
         else:
             error_label = tk.Label(self.content_frame, text="Please enter a valid item name and quantity")
             error_label.pack()
             self.root.after(1000, error_label.destroy)
 
-    def modify_item_quantity(self, list_id):
-        shopping_list = self.shopping_lists[list_id - 1]
-        modify_item_index = self.modify_item_index_entry.get()
-        modify_item_quantity = self.modify_item_quantity_entry.get()
-        if modify_item_index and modify_item_quantity.isdigit():
-            idx = int(modify_item_index) - 1
-            if 0 <= idx < len(shopping_list['items']):
-                shopping_list['items'][idx]['quantity'] = int(modify_item_quantity)
-                self.view_shopping_list()
-            else:
-                error_label = tk.Label(self.content_frame, text="Invalid item index")
-                error_label.pack()
-                self.root.after(1000, error_label.destroy)
-        else:
-            error_label = tk.Label(self.content_frame, text="Please enter a valid item index and quantity")
-            error_label.pack()
-            self.root.after(1000, error_label.destroy)
+# --------------------------------------------------------------
 
-    def delete_item(self, list_id):
-        shopping_list = self.shopping_lists[list_id - 1]
-        delete_item_index = self.delete_item_index_entry.get()
-        if delete_item_index and delete_item_index.isdigit():
-            idx = int(delete_item_index) - 1
-            if 0 <= idx < len(shopping_list['items']):
-                del shopping_list['items'][idx]
-                self.view_shopping_list()
-            else:
-                error_label = tk.Label(self.content_frame, text="Invalid item index")
-                error_label.pack()
-                self.root.after(1000, error_label.destroy)
-        else:
-            error_label = tk.Label(self.content_frame, text="Please enter a valid item index")
-            error_label.pack()
-            self.root.after(1000, error_label.destroy)
+    def get_list(self, id):
+        for shopping_list in self.shopping_lists:
+            if shopping_list.get("id") == id:
+                return shopping_list
+        return None
+    
+    def get_item(self, list_id, item_name):
+        shopping_list = self.get_list(list_id)
+        for item in shopping_list['items']:
+            if item.get("name") == item_name:
+                return item
+        return None
 
-    def delete_shopping_list(self, idx):
-        if 1 <= idx <= len(self.shopping_lists):
-            del self.shopping_lists[idx - 1]
-            self.show_admin()
+# --------------------------------------------------------------
 
 root = tk.Tk()
-app = ShoppingListApp(root)
+app = ArmazonGUI(root)
 root.mainloop()
