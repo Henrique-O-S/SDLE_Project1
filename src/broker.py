b887590f4d65e790d32d3f1dc42a4e4c2c955f84
Tiago Branquinho <tiagobranquinho1@gmail.com>
2023-12-01 18:52:05 +0000

    broker clean but problem replying to client

--- src/broker.py
+++ src/broker.py
@@ -43,38 +43,35 @@ class Broker:
             print("Waiting for message from client or server...")
             self.socks = dict(self.poller.poll(1000))
             self.frontend_polling()
-            self.backend_polling()
+            #self.backend_polling()
 
     def frontend_polling(self):
         if self.frontend_socket in self.socks and self.socks[self.frontend_socket] == zmq.POLLIN:
-            multipart_message = self.frontend_socket.recv_multipart()
-            print("ROUTER // Raw message from client | ", multipart_message)
-            client_id, dummy, message = multipart_message[0:]
-            message = json.loads(message.decode('utf-8'))
+            client_id, message = self.receiveMessage(self.frontend_socket)
             if message['action'] == 'get_shopping_list':
                 self.search_shopping_list(message['id'], client_id)
             elif message['action'] == 'crdts':
                 self.crdts_to_servers(message['crdt'], client_id)
 
-    def backend_polling(self):
+    """ def backend_polling(self):
         if self.backend_socket in self.socks and self.socks[self.backend_socket] == zmq.POLLIN:
-            multipart_message = self.backend_socket.recv_multipart()
-            print("DEALER // Raw message from server | ", multipart_message)
-            client_id, message = multipart_message[1], multipart_message[2]
+            client_id, message = self.receiveMessage(self.backend_socket)
             #message = json.loads(message.decode('utf-8'))
             #if (message['action']) == 'crdts':
             #    self.crdts_to_frontend(message['crdt'], client_id)
-            #self.frontend_socket.send_multipart([client_id, b"", message])
+            #self.frontend_socket.send_multipart([client_id, b"", message]) """
 
 
 # --------------------------------------------------------------
 
-    def search_shopping_list(self, id, client_id):
+    """ def search_shopping_list(self, id, client_id):
         server = MultiServer.get_server(id)
         self.backend_socket.connect(server.address)
         time.sleep(2)
         message = {'action': 'get_shopping_list', 'id': id}
-        self.backend_socket.send_multipart([b"", client_id, json.dumps(message).encode('utf-8')])
+        self.sendMessage(self.backend_socket, client_id, message)
+        self.backend_socket.disconnect(server.address) """
+
 
 # --------------------------------------------------------------
 
@@ -103,42 +100,53 @@ class Broker:
                 # Send the message to the server
                 crdt_json = crdt.to_json()
                 crdt_json['action'] = 'crdts'
-                self.backend_socket.send_multipart([b"", client_id, json.dumps(crdt_json).encode('utf-8')])
+                self.sendMessage(self.backend_socket, client_id, crdt_json)
                 print(server.address, crdt.to_json())
 
                 # Receive the response from the server
-                multipart_message = self.backend_socket.recv_multipart()
-                print("DEALER // Raw message from server | ", multipart_message)
-                client_identity, response = multipart_message[1], multipart_message[2]
+                client_id, response = self.receiveMessage(self.backend_socket)
                 responses.append(response)
                 # Disconnect from the server
                 self.backend_socket.disconnect(server.address)
                 time.sleep(1)
-        self.crdts_to_frontend(client_id)
+        self.crdts_to_client(client_id)
 
 
 # --------------------------------------------------------------
 
-    def crdts_to_frontend(self, client_id):
+    def crdts_to_client(self, client_id):
         #crdt_json['action'] = 'crdts'
         crdt_json = {'status': 'OK'}
-        message = json.dumps(crdt_json).encode('utf-8')
-        self.frontend_socket.send_multipart([client_id, b"", message])
-        #self.frontend_socket.send_multipart([client_id, b"", json.dumps(crdt_json).encode('utf-8')])
+        print("message sent")
+        self.sendMessage(self.frontend_socket, client_id, crdt_json)
 
 # --------------------------------------------------------------
     def pulse_check_to_servers(self):
         for server in MultiServer.servers:
             self.backend_socket.connect(server.address)
             message = {'action': 'r_u_there'}
-            self.backend_socket.send_multipart([b"", b"", json.dumps(message).encode('utf-8')])
+            self.sendMessage(self.backend_socket, b"", message)
 
-            multipart_message = self.backend_socket.recv_multipart()
-            client_identity, response = multipart_message[1], multipart_message[2]
-            response = json.loads(response.decode('utf-8'))
+            _, response = self.receiveMessage(self.backend_socket)
             if response['status'] == 'OK':
                 print('PULSE CONFIRMED', server.address)
             else:
                 print('backend response: NOT OK')
 
             self.backend_socket.disconnect(server.address)
+    
+
+
+    def receiveMessage(self, socket):
+        if socket == self.frontend_socket:
+            source = 'client'
+        else:
+            source = 'server'
+        multipart_message = socket.recv_multipart()
+        print("Raw message from ", source, " | ", multipart_message)
+        client_id, message = multipart_message[1], multipart_message[2]
+        message = json.loads(message.decode('utf-8'))
+        return client_id, message
+
+    def sendMessage(self, socket, client_id, message):
+        socket.send_multipart([client_id, b"", json.dumps(message).encode('utf-8')])
